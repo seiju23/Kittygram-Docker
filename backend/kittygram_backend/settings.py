fe2a2a42e3a76869373e2f76a7d9af778e87de08
Igor Ravlis <seiju.pioneer23@gmail.com>
2023-08-20 21:01:52 +0000

    some commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', '')
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
